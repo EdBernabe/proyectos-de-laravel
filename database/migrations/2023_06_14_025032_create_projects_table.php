0edfc456aa5f1e672d6a6c7611a616acbeecf438
EdBernabe <3dgardo1993@gmail.com>
2023-06-13 23:35:07 -0600

    0.1

---
+++ database/migrations/2023_06_14_025032_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->string('nombreProyecto',100);
+            $table->string('fuenteFondos',100);
+            $table->string('montoPlanificado',255);
+            $table->string('montoPatrocinado',255);
+            $table->string('montoFondosPropios', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
